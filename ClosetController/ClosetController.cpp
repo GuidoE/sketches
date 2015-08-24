54b61a7611f3b1add764f81e466e23a6ff15504c
Guido Espinosa <guido@superg.co>
2015-08-23 20:08:41 -0500

    adds the ClosetController sketch

---
+++ ClosetController/ClosetController.cpp
@@ -0,0 +1,49 @@
+// Do not remove the include below
+#include "ClosetController.h"
+#include "PowerTail.h"
+#include "Arduino.h"
+#include "PIR.h"
+
+PowerTail tail(A1, 13);
+PIR pir(7, 13, 10);
+
+boolean action;
+
+void setup() {
+	pinMode(12, OUTPUT);
+
+	digitalWrite(12, HIGH);
+	Serial.begin(9600);
+	Serial.print("calibrating sensor ");
+	for (int i = 0; i < pir.calibrationTime; i++) {
+		Serial.print(".");
+		delay(1000);
+	}
+	Serial.println(" done");
+	Serial.println("PIR sensor active!");
+
+	Serial.println("starting up Power Tail!");
+	delay(50);
+	action = false;
+}
+
+void loop() {
+	pir.Motion();
+	/*if (pir.presence == true && action != action) {
+		tail.ManualPower();
+		action = !action;
+	} else if (pir.presence == true && tail.powerOn == true) {
+		//Serial.println("Still sensing presence");
+	} else {
+		tail.ManualPower();
+		action = !action;
+	}*/
+	if (pir.presence && action == false) {
+		tail.ManualPower();
+		action = true;
+	} else if (!pir.presence && action == true) {
+		tail.ManualPower();
+		action = false;
+	}
+
+}
---
+++ ClosetController/ClosetController.h
@@ -0,0 +1,27 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _ClosetController_H_
+#define _ClosetController_H_
+#include "Arduino.h"
+//add your includes for the project ClosetController here
+#include "PIR.h"
+#include "PowerTail.h"
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project ClosetController here
+
+
+//Do not add code below this line
+#endif /* _ClosetController_H_ */
